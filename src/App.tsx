b1b72c1acf84a2f1291630914f513b37a77d9f8e
AwalHossain <waqin21@gmail.com>
2022-01-26 08:47:20 +0600

    Typescript add cart item project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Drawer from "@material-ui/core/Drawer";
+import { AddShoppingCart } from "@material-ui/icons";
+import Grid from "@material-ui/core/Grid";
+import Badge from "@material-ui/core/Badge";
+import { Wrapper } from "./App.styles";
+import { useQuery } from "react-query";
+import { LinearProgress } from "@material-ui/core";
 
-function App() {
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  descripttion: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  const getTotalItems = () => null;
+  const handleAddToCart = () => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong ...</div>;
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Start from here</h1>
     </div>
   );
-}
+};
 
 export default App;
